90f4ba0486e837d453b9a8341c50b48bfd1e88e9
RichardHpa <richard.hpa@hotmail.com>
2019-08-20 20:20:30 +1200

    preparing project

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const allProducts = require('./data/products');
+
+app.use(function(req, res, next){
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
+app.get('/', function(req, res){
+    res.send('Welcome to our Products API. Use endpoints to filter out the data');
+});
+
+
+
+app.listen(port, () => {
+    console.clear();
+    console.log(`application is running on port ${port}`)
+});
