456868e209f10c85ba352f9766a6fe80f4244817
ryleybridges <rye.brid@gmail.com>
2019-08-21 11:52:16 +1200

    Gotten product from ID working

--- server.js
+++ server.js
@@ -13,9 +13,38 @@ app.get('/', function(req, res){
     res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
 
+app.get('/allProducts', function(req, res){
+  res.send(allProducts);
+});
 
+app.get('/product/:id', function(req, res){
+  const productID = req.params.id;
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+        if(allProducts[i].id.toString() === productID){
+            filteredData.push(allProducts[i]);
+        }
+    }
+    res.send(filteredData);
+});
+
+app.get('/product/delete/:id', function(req, res){
+  const productID = req.params.id;
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+        if(allProducts[i].id.toString() === productID){
+            productID.splice(allProducts[i]);
+            console.log(productID);
+        }
+    }
+    res.send(filteredData);
+});
+
+// app.get('/product/edit/:id', function(req, res){
+//
+// });
 
 app.listen(port, () => {
     console.clear();
-    console.log(`application is running on port ${port}`)
+    console.log(`application is running on port ${port}`);
 });
