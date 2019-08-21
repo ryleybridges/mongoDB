f8a345e6d9c75e534d44bb564242fbc83b4bbe3f
ryleybridges <rye.brid@gmail.com>
2019-08-22 11:51:31 +1200

    Added things to server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
@@ -18,37 +24,47 @@ app.get('/allProducts', function(req, res){
 });
 
 app.get('/product/:id', function(req, res){
-  const productID = req.params.id;
-  let filteredData = [];
+  const id = req.params.id;
   for (var i = 0; i < allProducts.length; i++) {
-        if(allProducts[i].id.toString() === productID){
-            filteredData.push(allProducts[i]);
+        if(id == allProducts[i].id){
+            res.send(allProducts[i]);
+            break;
         }
     }
-    res.send(filteredData);
 });
 
-app.get('/product/delete/:id', function(req, res){
-  const productID = req.params.id;
-  let filteredData = [];
-  for (var i = 0; i < allProducts.length; i++) {
-        if(allProducts[i].id.toString() === productID){
-            filteredData.push(allProducts[i]);
-        }
-    }
-    res.send(filteredData);
-});
+app.post('/product', function(req, res){
+  // console.log('a post request has been made');
+  // console.log(req.body);
+  let product = {
+    name: req.body.name,
+    price: req.body.price,
+    message: 'We are about to send this product to a database'
+  }
+  res.send(product);
+})
 
-app.get('/product/edit/:id', function(req, res){
-  const productID = req.params.id;
-  let filteredData = [];
-  for (var i = 0; i < allProducts.length; i++) {
-        if(allProducts[i].id.toString() === productID){
-            filteredData.push(allProducts[i]);
-        }
-    }
-    res.send(filteredData);
-});
+// app.get('/product/delete/:id', function(req, res){
+//   const productID = req.params.id;
+//   let filteredData = [];
+//   for (var i = 0; i < allProducts.length; i++) {
+//         if(allProducts[i].id.toString() === productID){
+//             filteredData.push(allProducts[i]);
+//         }
+//     }
+//     res.send(filteredData);
+// });
+//
+// app.get('/product/edit/:id', function(req, res){
+//   const productID = req.params.id;
+//   let filteredData = [];
+//   for (var i = 0; i < allProducts.length; i++) {
+//         if(allProducts[i].id.toString() === productID){
+//             filteredData.push(allProducts[i]);
+//         }
+//     }
+//     res.send(filteredData);
+// });
 
 app.listen(port, () => {
     console.clear();
