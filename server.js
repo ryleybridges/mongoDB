114f0f5aeb6ccc44672273760c571b40e124c2c6
ryleybridges <rye.brid@gmail.com>
2019-08-25 20:19:54 +1200

    Updated

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./config.json');
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0-zd20o.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('we are connected to mongo db');
+});
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
@@ -13,9 +31,40 @@ app.get('/', function(req, res){
     res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
 
+app.get('/allProducts', function(req, res){
+    res.send(allProducts);
+})
 
 
-app.listen(port, () => {
-    console.clear();
-    console.log(`application is running on port ${port}`)
+app.get('/product/:id', function(req, res){
+    const id = req.params.id
+    for (var i = 0; i < allProducts.length; i++) {
+        if(id == allProducts[i].id){
+            res.send(allProducts[i]);
+            break;
+        }
+    }
+})
+
+
+const Product = require('./models/products');
+
+app.post('/product', function(req, res){
+    // console.log('a post request has been made');
+    // console.log(req.body);
+    // let product = {
+    //     name: req.body.name,
+    //     price: req.body.price,
+    //     message: 'We are about to send this product to a database'
+    // };
+    // res.send(product);
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    product.save().then(result => {
+        res.send(result);
+    }).catch(err => res.send(err));
 });
