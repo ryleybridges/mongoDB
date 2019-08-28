b5f847a49aa5e674117865288b8d8ccf95545a38
ryleybridges <rye.brid@gmail.com>
2019-08-29 09:41:44 +1200

    Updated

--- models/products.js
+++ models/products.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  price: Number
-=======
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
     price: Number
->>>>>>> master
 });
 
 module.exports = mongoose.model('Product', productSchema);
